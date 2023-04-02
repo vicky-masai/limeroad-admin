f35ff5fa7372b68933cc976ed0e6119129a7cefc
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-04-02 17:02:17 +0530

    admin

--- .next/static/chunks/pages/deleted-e1664df10dbd353e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[342],{70:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/deleted",function(){return u(6118)}])},6118:function(){}},function(n){n.O(0,[774,888,179],function(){return n(n.s=70)}),_N_E=n.O()}]);
